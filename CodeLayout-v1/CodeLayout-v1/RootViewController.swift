71d7e6ea9a5a22fa61efbdfd2f047edd4c1d7c93
AnatolYarmolovich <yarmolovich1986@gmail.com>
2023-10-07 22:33:39 +0200

    CodeLayout-v2 added

--- CodeLayout-v1/CodeLayout-v1/RootViewController.swift
+++ CodeLayout-v1/CodeLayout-v1/RootViewController.swift
@@ -2,28 +2,10 @@
 //  RootViewController.swift
 //  CodeLayout-v1
 //
-//  Created by Anatol Yarmalovich on 05/10/2023.
+//  Created by Anatol Yarmalovich on 07/10/2023.
 //
 
 import UIKit
 
 class RootViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
