9a8c6b4aa09b49eb8c394e21b031a2b053181062
AnatolYarmolovich <yarmolovich1986@gmail.com>
2023-10-07 23:04:08 +0200

    CodeLayout-v5

---
+++ CodeLayout-v5/CodeLayout-v5/RootViewController.swift
@@ -0,0 +1,19 @@
+//
+//  RootViewController.swift
+//  CodeLayout-v5
+//
+//  Created by Anatol Yarmalovich on 07/10/2023.
+//
+
+import UIKit
+
+class RootViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
