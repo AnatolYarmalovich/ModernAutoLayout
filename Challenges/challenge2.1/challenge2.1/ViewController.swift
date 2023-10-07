7977318068fa161be920ac3567396c4a2b91a30f
AnatolYarmolovich <yarmolovich1986@gmail.com>
2023-10-07 23:16:50 +0200

    Challenges/challenge2.1

---
+++ Challenges/challenge2.1/challenge2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  challenge2.1
+//
+//  Created by Anatol Yarmalovich on 07/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
