acd646734b6f571d8022ee4b3f3e0121baac7553
AnatolYarmolovich <yarmolovich1986@gmail.com>
2023-10-08 22:00:35 +0200

    TwoButtons-v1

---
+++ TwoButtons-v1/TwoButtons-v1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwoButtons-v1
+//
+//  Created by Anatol Yarmalovich on 08/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
